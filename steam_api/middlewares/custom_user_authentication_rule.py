e56d37a574ce295ffcaace1ca42c99f102ae0a02
thuannt <thuannt@ghn.vn>
2025-10-11 11:46:18 +0700

    update

--- steam_api/middlewares/custom_user_authentication_rule.py
+++ steam_api/middlewares/custom_user_authentication_rule.py
@@ -1,6 +1,4 @@
-
 from steam_api.models.web_user import WebUser, WebUserStatus
-
 
 def custom_user_authentication_rule(user: WebUser) -> bool:
     return user is not None and user.status == WebUserStatus.ACTIVATED
